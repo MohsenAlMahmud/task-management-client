6d57b9ccf0b0f51d9106f10d1f26675b29278120
MohsenAlMahmud <mohsen.al.mahmud@gmail.com>
2023-12-22 05:29:31 +0600

    complete to-do page

--- src/Dashboard/AddTask.jsx
+++ src/Dashboard/AddTask.jsx
@@ -1,4 +1,3 @@
-
 import { useContext } from "react";
 import Swal from "sweetalert2";
 import axios from "axios";
@@ -9,36 +8,35 @@ import { useNavigate } from "react-router-dom";
 const AddTask = () => {
 
     const navigate = useNavigate()
-    const {user} = useContext(AuthContext);
-    const handleTask = async(e) => {
+    const { user } = useContext(AuthContext);
+    const handleTask = async (e) => {
         e.preventDefault();
         const form = e.target;
         const name = form.name.value;
         const email = form.email.value;
-        const tittle = form.tittle.value;
-        // const image = form.image.value;
-        // const category = form.category.value;
-        // const shortDescription = form.shortDescription.value;
+        const tittle = form.tittle.value;       
         const taskDescription = form.taskDescription.value;
+        const taskDeadline = form.taskDeadline.value;
+        const priority = form.priority.value;
         const status = 'to-do';
-        const taskData = { name, email, tittle, taskDescription, status };
+        const taskData = { name, email, tittle, taskDescription, taskDeadline, priority, status };
         console.log(taskData);
 
-        
+
         try {
             const response = await axios.post("http://localhost:5000/tasks", taskData, {
-              headers: {
-                'Content-Type': 'application/json',
-              },
+                headers: {
+                    'Content-Type': 'application/json',
+                },
             });
             console.log(response.data);
             Swal.fire('Task Submitted Successfully')
             form.reset();
             navigate('/dashboard/write-task');
-          } 
-          catch (error) {
+        }
+        catch (error) {
             console.log(error);
-          }
+        }
     }
 
     return (
@@ -63,39 +61,30 @@ const AddTask = () => {
                         <span className="label-text text-lg font-medium">Tittle</span>
                     </label>
                     <input type="text" name="tittle" placeholder="Tittle" className="input input-bordered" />
-                </div>
-                {/* <div className="form-control">
-                    <label className="label">
-                        <span className="label-text text-lg font-medium">Insert Image URL</span>
-                    </label>
-                    <input type="text" name="image" placeholder="Image" className="input input-bordered" />
-                </div> */}
-                {/* <div className="form-control">
-                    <label className="label">
-                        <span className="label-text text-lg font-medium">Category</span>
-                    </label>                    
-                    <select name="category" className="select select-bordered w-full">
-                        <option defaultValue="">Pick your Travel Category</option>
-                        <option value="Solo Adventure">Solo Adventure</option>
-                        <option value="Family Exploration">Family Exploration</option>
-                        <option value="Romantic Gateway">Romantic Getaway</option>
-                        <option value="Volunteer and Humanitarian Trips">Volunteer and Humanitarian Trips</option>
-                        <option value="Group Travel">Group Travel</option>
-                        <option value="Business Travel">Business Travel</option>
-                    </select>
-                </div> */}
-                {/* <div className="form-control">
-                    <label className="label">
-                        <span className="label-text text-lg font-medium">Short Description</span>
-                    </label>
-                    <input type="text" name="shortDescription" placeholder="Description" className="input input-bordered" />
-                </div> */}
+                </div>                
 
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text text-lg font-medium">Task Description</span>
                     </label>
                     <input type="text" name="taskDescription" placeholder="Task Description" className="input input-bordered" />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text text-lg font-medium">Task Deadline</span>
+                    </label>
+                    <input type="text" name="taskDeadline" placeholder="Task Deadline" className="input input-bordered" />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text text-lg font-medium">Priority</span>
+                    </label>
+                    <select name="priority" className="select select-bordered w-full">
+                        <option defaultValue="">Pick your Task Priority</option>
+                        <option value="Low">Low</option>
+                        <option value="Moderate">Moderate</option>
+                        <option value="High">High</option>
+                    </select>
                 </div>
 
 
--- src/Dashboard/WriteTask.jsx
+++ src/Dashboard/WriteTask.jsx
@@ -1,42 +1,115 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
+const apiUrl = 'http://localhost:5000/tasks';
 
 const WriteTask = () => {
+  const [tasks, setTasks] = useState([]);
+  const [statusList, setStatusList] = useState(['to-do', 'on-going', 'completed']);
 
-    const tasks = useLoaderData();
+  useEffect(() => {
+    fetchTasks();
+  }, []);
 
-    const formatDateTime = (dateTimeString) => {
-        const dateTime = new Date(dateTimeString);
-        const formattedDate = dateTime.toLocaleDateString();
-        const formattedTime = dateTime.toLocaleTimeString();
-        return { formattedDate, formattedTime };
-    };
+  const fetchTasks = async () => {
+    try {
+      const response = await axios.get(apiUrl);
+      setTasks(response.data);
+    } catch (error) {
+      console.error('Error fetching tasks:', error);
+    }
+  };
 
-    return (
-        <div>
-            <h2>Write your task here</h2>
-            <div className="w-full text-right">
-                <Link to="add-task"><button className="btn btn-accent">Add Task</button></Link>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                {tasks.map((task) =>
-                    <div key={task._id}>
-                        <div className="card w-96 bg-neutral text-neutral-content">
-                            <div className="card-body items-center text-center">
-                                <div className="flex w-full justify-between">
-                                    <h2 className="">{formatDateTime(task.createdAt).formattedDate}</h2>
-                                    <h2 className="">{formatDateTime(task.createdAt).formattedTime}</h2>
-                                </div>
-                                <h2 className="card-title">{task.tittle}</h2>
-                                <h2 className="">{task.taskDescription}</h2>                                
-                                <h2 className="">{task.status}</h2>                                
-                            </div>
-                        </div>
+  const formatDateTime = (dateTimeString) => {
+    const dateTime = new Date(dateTimeString);
+    const formattedDate = dateTime.toLocaleDateString();
+    const formattedTime = dateTime.toLocaleTimeString();
+    return { formattedDate, formattedTime };
+  };
+
+  const handleStatusChange = async (taskId, newStatus) => {
+    const updatedTasks = tasks.map((task) => (task._id === taskId ? { ...task, status: newStatus } : task));
+
+    try {
+      await axios.put(`${apiUrl}/${taskId}`, { status: newStatus });
+      setTasks(updatedTasks);
+    } catch (error) {
+      console.error('Error updating task status:', error);
+    }
+  };
+
+  const handleDeleteTask = async (taskId) => {
+    try {
+      await axios.delete(`${apiUrl}/${taskId}`);
+      const updatedTasks = tasks.filter((task) => task._id !== taskId);
+      setTasks(updatedTasks);
+      alert('Task deleted successfully!');
+    } catch (error) {
+      console.error('Error deleting task:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Write your task here</h2>
+      <div className="w-full text-right">
+        <Link to="add-task">
+          <button className="btn btn-accent">Add Task</button>
+        </Link>
+      </div>
+      <div className="grid grid-cols-3">
+        {statusList.map((status) => (
+          <div key={status} className="grid grid-cols-1">
+            <h2 className="text-3xl font-bold pb-4">{capitalizeFirstLetter(status)} List</h2>
+            {tasks
+              .filter((task) => task.status === status)
+              .map((task) => (
+                <div key={task._id} className="pb-4 pr-6">
+                  <div className="card bg-neutral text-neutral-content">
+                    <div className="card-body items-center text-center">
+                      <div className="flex w-full justify-between">
+                        <h2>{formatDateTime(task.createdAt).formattedDate}</h2>
+                        <h2>{formatDateTime(task.createdAt).formattedTime}</h2>
+                      </div>
+                      <h2 className="card-title">{task.tittle}</h2>
+                      <h2>{task.taskDescription}</h2>
+                      <h2>Deadline : {task.taskDeadline}</h2>
+                      <h2>Priority : {task.priority}</h2>
+                      <div>
+                        <label htmlFor={`status-${task._id}`}>Status:</label>
+                        <select
+                          className='btn btn-xs btn-accent ml-2'
+                          id={`status-${task._id}`}
+                          value={task.status}
+                          onChange={(e) => handleStatusChange(task._id, e.target.value)}
+                        >
+                          {statusList.map((statusOption) => (
+                            <option key={statusOption} value={statusOption}>
+                              {capitalizeFirstLetter(statusOption)}
+                            </option>
+                          ))}
+                        </select>
+                      </div>
+                      <button
+                        className="btn btn-error mt-2"
+                        onClick={() => handleDeleteTask(task._id)}
+                      >
+                        Delete
+                      </button>
                     </div>
-                )}
-            </div>
-        </div>
-    );
+                  </div>
+                </div>
+              ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default WriteTask;
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
